b88020fbb0e5fa2571367a22fa8bae83bdd10b33
vikas924 <obroy902@gmail.com>
2023-07-06 15:34:14 +0530

    Add handleclick function

--- src/components/calculatorui.js
+++ src/components/calculatorui.js
@@ -1,51 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
 function CalcUi() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    // Access the event object (e) here
+    const value = e.target.innerHTML;
+    setState(calculate(state, value));
+  };
+
   return (
     <>
       <div className="display">
-        <span className="display-result">0</span>
+        <span className="display-result">{state.next || state.operation || state.total || 0}</span>
       </div>
       <div className="keypad">
         <div className="rows">
-          <Calcbutton symbol="AC" buttonclass="key-silver" />
-          <Calcbutton symbol="+/-" buttonclass="key-silver" />
-          <Calcbutton symbol="%" buttonclass="key-silver" />
-          <Calcbutton symbol="÷" buttonclass="key-orange" />
+          <Calcbutton symbol="AC" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="+/-" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="%" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="÷" funct={handleClick} buttonclass="key-orange" />
         </div>
         <div className="rows">
-          <Calcbutton symbol="7" buttonclass="key-silver" />
-          <Calcbutton symbol="8" buttonclass="key-silver" />
-          <Calcbutton symbol="9" buttonclass="key-silver" />
-          <Calcbutton symbol="+" buttonclass="key-orange" />
+          <Calcbutton symbol="7" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="8" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="9" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="x" funct={handleClick} buttonclass="key-orange" />
         </div>
         <div className="rows">
-          <Calcbutton symbol="4" buttonclass="key-silver" />
-          <Calcbutton symbol="5" buttonclass="key-silver" />
-          <Calcbutton symbol="6" buttonclass="key-silver" />
-          <Calcbutton symbol="-" buttonclass="key-orange" />
+          <Calcbutton symbol="4" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="5" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="6" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="-" funct={handleClick} buttonclass="key-orange" />
         </div>
         <div className="rows">
-          <Calcbutton symbol="1" buttonclass="key-silver" />
-          <Calcbutton symbol="2" buttonclass="key-silver" />
-          <Calcbutton symbol="3" buttonclass="key-silver" />
-          <Calcbutton symbol="+" buttonclass="key-orange" />
+          <Calcbutton symbol="1" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="2" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="3" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="+" funct={handleClick} buttonclass="key-orange" />
         </div>
         <div className="rowchange">
-          <Calcbutton symbol="0" buttonclass="key-silver" />
-          <Calcbutton symbol="." buttonclass="key-silver" />
-          <Calcbutton symbol="=" buttonclass="key-orange" />
+          <Calcbutton symbol="0" funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="." funct={handleClick} buttonclass="key-silver" />
+          <Calcbutton symbol="=" funct={handleClick} buttonclass="key-orange" />
         </div>
       </div>
     </>
   );
 }
 
-function Calcbutton({ buttonclass, symbol }) {
+function Calcbutton({ buttonclass, symbol, funct }) {
   return (
     <>
-      <button type="button" className={buttonclass}>{symbol}</button>
+      <button type="button" onClick={funct} className={buttonclass}>{symbol}</button>
     </>
   );
 }
@@ -56,6 +69,7 @@ Calcbutton.propTypes = {
     PropTypes.string,
     PropTypes.number,
   ]).isRequired,
+  funct: PropTypes.func.isRequired,
 };
 
 export default CalcUi;
