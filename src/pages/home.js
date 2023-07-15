6f90e34cadd4a29bb619342652113958508645f1
vikas924 <obroy902@gmail.com>
2023-07-15 11:45:08 +0530

    Add pages and styles

---
+++ src/pages/home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="page">
+      <h3 className="heading">Welcome to our page!</h3>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+        quidem? Doloribus doloremque id, magni unde quibusdam omnis provident
+        debitis. Incidunt deserunt consequatur, numquam placeat asperiores id
+
+        ipsum? Labore atque velit error distinctio nam iusto
+        voluptatem! At, hic. Temporibus consectetur, debitis eum vero,
+        reiciendis corporis, sapiente iusto magnam ullam voluptates ea! ipsum?
+        Labore atque velit error distinctio
+      </p>
+      <span className="paragraph2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        vero recusandae ratione magni corporis odio consequatur eos. Illum, ut
+        perferendis. ipsum? Labore atque velit error distinctio nam iusto
+        voluptatem! At, hic. Temporibus consectetur, debitis eum vero,
+        reiciendis corporis, sapiente iusto magnam ullam voluptates ea!
+      </span>
+    </div>
+  );
+}
---
+++ src/pages/quotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Quotes from '../components/quote';
+
+export default function Quot() {
+  return (
+    <>
+      <Quotes />
+    </>
+  );
+}
